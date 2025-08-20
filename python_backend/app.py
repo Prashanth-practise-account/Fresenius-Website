93e0ec473cfb0122de7654db1e13d45f73de489c
your name <your-email@example.com>
2025-08-20 13:39:55 +0530

    updateD

---
+++ python_backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder="../build", static_url_path="/")
+
+# Serve React page
+@app.route("/")
+def index():
+    return send_from_directory(app.static_folder, "index.html")
+
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
